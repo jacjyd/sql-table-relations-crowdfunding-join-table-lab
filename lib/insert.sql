227d06805fc5086737181c155f5902e16b56c79a
Diana Liao <dijali@gmail.com>
2021-03-02 23:09:55 -0500

    commit tree 445df759ab0d2a8f40fa6a764f59e50d01fdc719

---
+++ lib/insert.sql
@@ -0,0 +1,71 @@
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) 
+VALUES 
+("Wolves-only Highway", "public installation", 500000, "2/17/2000", "6/1/2000"),
+("Clawdy Day Cat Cafe", "retail", 300000, "5/24/2010", "7/6/2010"),
+("Adoption: the Board Game", "entertainment", 3000, "3/10/2015", "5/1/2015"),
+("Death: the Board Game", "entertainment", 4000, "2/17/2005", "4/1/2005"),
+("Leaf-E, a virtual plant", "entertainment", 10000, "8/17/2010", "10/1/2010"),
+("Mochi Mundo", "retail", 800000, "2/17/2012", "3/22/2012"),
+("Cactus Seeking Hugs", "entertainment", 3500, "11/17/2014", "12/20/2014"),
+("Constant Christmas", "public installation", 122500, "2/17/2020", "6/1/2020"),
+("There are no more ideas", "entertainment", 200, "2/1/2020", "3/1/2020"),
+("Data Sets 4 U", "retail", 5000000, "1/22/2021", "2/22/2021")
+;
+
+
+INSERT INTO users (name, age) VALUES 
+("Macky", 40),
+("Zack", 31),
+("Jen", 38),
+("Andrew", 36),
+("Sara", 10),
+("Cory", 11),
+("Dwayne", 18),
+("Ellen", 73),
+("Paul", 35),
+("Olivia", 20),
+("Steven", 32),
+("Alec", 36),
+("Sydney", 31),
+("Erica", 34),
+("Walter", 65),
+("Jack", 78),
+("Thomas", 33),
+("Cameron", 32),
+("Jen", 32),
+("Jenn", 35)
+;
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES 
+(50,1,1),
+(20,6,1),
+(65,1,10),
+(10,1,1),
+(5,1,2),
+(100,1,2),
+(97,1,3),
+(23,1,3),
+(54,1,4),
+(17,1,5),
+(19,1,4),
+(31,1,9),
+(22,1,4),
+(45,1,4),
+(140,1,5),
+(200,1,5),
+(195,1,5),
+(76,1,5),
+(81,1,6),
+(87,1,6),
+(53,1,7),
+(48,1,7),
+(49,1,7),
+(51,1,7),
+(52,1,7),
+(53,1,7),
+(54,1,7),
+(56,1,8),
+(59,1,9),
+(1000,1,10)
+;
